d4c0c87d887d5c3ad0407bb81d8b971d65427c70
moonkabir <moon@pencilbox.edu.bd>
2020-12-20 17:17:07 +0600

    21.3 widget media attachment and javascript event

--- wp-content/plugins/DemoWidgets/demowidget.php
+++ wp-content/plugins/DemoWidgets/demowidget.php
@@ -11,6 +11,7 @@
  */
 
 require_once plugin_dir_path(__FILE__)."widgets/class.demowidget.php";
+require_once plugin_dir_path(__FILE__)."widgets/class.advertisement.php";
 
 function demowidget_load_textdomain(){
     load_plugin_textdomain('demowidget', false, plugin_dir_path(__FILE__)."languages/");
@@ -19,5 +20,14 @@ add_action('plugins_loaded', 'demowidget_load_textdomain');
 
 function demowidget_register(){
     register_widget('DemoWidget');
+    register_widget('AdvertisementWidget');
 }
 add_action('widgets_init', 'demowidget_register');
+
+function demowidget_admin_enqueue_scripts($screen){
+    if($screen = 'widgets.php'){
+        wp_enqueue_media();
+        wp_enqueue_script("advertisement-widget-js", plugin_dir_url(__FILE__)."js/media-gallery.js");
+    }
+}
+add_action('admin_enqueue_scripts', 'demowidget_admin_enqueue_scripts');
