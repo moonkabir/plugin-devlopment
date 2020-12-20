47a0cb03db572cbf060bee297f039820d01fae6c
moonkabir <moon@pencilbox.edu.bd>
2020-12-20 16:24:23 +0600

    21.2 widget update function and input validation & verifications

--- wp-content/plugins/DemoWidgets/widgets/class.demowidget.php
+++ wp-content/plugins/DemoWidgets/widgets/class.demowidget.php
@@ -14,6 +14,7 @@ class DemoWidget extends WP_Widget {
 		$title     = isset( $instance['title'] ) ? $instance['title'] : __( 'Demo Widget', 'demowidget' );
 		$latitude  = isset( $instance['latitude'] ) ? $instance['latitude'] : 23.9;
 		$longitude = isset( $instance['longitude'] ) ? $instance['longitude'] : 90.8;
+		$email = isset( $instance['email'] ) ? $instance['email'] : 'john@doe.com';
 		?>
         <p>
             <label for="<?php echo esc_attr( $this->get_field_id( 'title' ) ); ?>"><?php _e( 'Title', 'demowidget' ) ?></label>
@@ -33,11 +34,17 @@ class DemoWidget extends WP_Widget {
                    name="<?php echo esc_attr( $this->get_field_name( 'longitude' ) ) ?>"
                    value="<?php echo esc_attr( $longitude ); ?>">
         </p>
+        <p>
+            <label for="<?php echo esc_attr( $this->get_field_id( 'email' ) ); ?>"><?php _e( 'Email', 'demowidget' ) ?></label>
+            <input class="widefat" type="text" id="<?php echo esc_attr( $this->get_field_id( 'email' ) ); ?>"
+                   name="<?php echo esc_attr( $this->get_field_name( 'email' ) ) ?>"
+                   value="<?php echo esc_attr( $email ); ?>">
+        </p>
 		<?php
 	}
 
 	public function widget( $args, $instance ) {
-		// print_r($args);
+		//print_r($args);
 
 		echo $args['before_widget'];
 		if ( isset( $instance['title'] ) && $instance['title'] != '' ) {
@@ -55,9 +62,22 @@ class DemoWidget extends WP_Widget {
 
 	}
 
-	/*
-	public function update() {
+	public function update($new_instance, $old_instance) {
 
-	}*/
+		$instance = $new_instance;
+		$instance['title'] = sanitize_text_field($instance['title']);
+
+		$email = $new_instance['email'];
+		if(!is_email($email)){
+			$instance['email'] = $old_instance['email'];
+		}
+		if(!is_numeric($new_instance['latitude'])){
+			$instance['latitude'] = $old_instance['latitude'];
+        }
+        if(!is_numeric($new_instance['longitude'])){
+			$instance['longitude'] = $old_instance['longitude'];
+        }
+		return $instance;
+	}
 }
 
